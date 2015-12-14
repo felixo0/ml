51eb9eba5ada2f367ae905215ad6e68e4c8e19e1
LogicWang <mangobada@163.com>
2015-12-14 20:00:46 +0800

    add a new module to parse chinese

--- clusters.py
+++ clusters.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+#LogicWang
+#2015.12.01
 from PIL import Image, ImageDraw
 from math import sqrt
+
+import random
+
 def readfile(filename):
     lines=[line for line in open(filename)]
 
@@ -144,3 +150,35 @@ def rotatematrix(data):
         newrow=[data[j][i] for j in range(len(data))]
         newdata.append(newrow)
     return newdata
+
+def kcluster(rows,distance=pearson,k=4):
+    ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches=None
+    for t in range(100):
+        print('Iteration %d' %t)
+        bestmatches=[ [] for i in range(k)]
+
+        for j in range(len(rows)):
+            row=rows[j]
+            bestmatch=0
+            for i in range(k):
+                d=distance(clusters[i],row)
+                if d<distance(clusters[bestmatch],row):
+                    bestmatch=i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches==lastmatches:break
+        lastmatches=bestmatches
+        for i in range(k):
+            avgs=[0.0]*len(rows[0])
+            if len(bestmatches[i]) >0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m]+=rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j]/=len(bestmatches[i])
+                clusters[i]=avgs
+    return bestmatches
