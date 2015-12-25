be8307969052961057ede66bf164e608a285731c
LogicWang <mangobada@163.com>
2015-12-25 20:43:39 +0800

    add crawler

--- searchengine.py
+++ searchengine.py
@@ -1,7 +1,18 @@
-#
+#!/usr/bin/env python3
 #Author@LogicWang
 #2015.12.10
-#
+'''
+this file include two class 
+crawler to download,parse pages, and then save to database
+
+'''
+
+from urllib import request, parse
+from bs4 import *
+
+#the words to ignore
+ignoreword=set(['the','of','to','and','a','in','is','it'])
+
 class crawler:
     def __init__(self,dbname):
         pass
@@ -15,7 +26,7 @@ class crawler:
         return None
     # Index an individual page
     def addtoindex(self,url,soup):
-        print 'Indexing %s' % url
+        print('Indexing %s' %url)
     # Extract the text from an HTML page (no tags)
     def gettextonly(self,soup):
         return None
@@ -32,6 +43,30 @@ class crawler:
     # first search to the given depth, indexing pages
     # as we go
     def crawl(self,pages,depth=2):
+        for i in range(depth):
+            newpages=set()
+            for page in pages:
+                try:
+                    c = request.urlopen(page)
+                except:
+                    print("Could not open %s" %page)
+                    continue
+                soup=BeautifulSoup(c.read())
+                self.addtoindex(page,soup)
+
+                links = soup('a')
+                for link in links:
+                    if('href' in dict(link.attrs)):
+                        url=parse.urljoin(page,link['href'])
+                        if url.find("'")!=-1:
+                            continue
+                        url=url.split('#')[0]
+                        if url[0:4]=='http' and not self.isindexed(url):
+                            newpages.add(url)
+                        linkText=self.gettextonly(link)
+                        self.addlinkref(page,url,linkText)
+            self.dbcommit()
+            pages=newpages
         pass
     # Create the database tables
     def createindextables(self):
